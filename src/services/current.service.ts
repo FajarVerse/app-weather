3b242048c09961126db20f1019730c0fbf7560a6
FajarVerse <gifarifajarm174@gmail.com>
2024-12-06 21:20:35 +0700

    mencoba memanggil api

---
+++ src/services/current.service.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getCurrent = async (lat: number, long: number) => {
+  try {
+    const response = await axios.get(
+      `http://api.weatherapi.com/v1/forecast.json?key=a407e6f112e44f8b84e121933240412&q=${lat},${long}`
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
